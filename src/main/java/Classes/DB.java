6dc37a162d4e6eaa3293e2ef801797e29c84e6a3
weder-monteiro <weder_monteiro@outlook.com.br>
2019-05-19 14:25:05 -0300

    Recuperando dados em department

--- src/main/java/Classes/DB.java
+++ src/main/java/Classes/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import Exceptions.DbException;
@@ -43,6 +45,26 @@ public class DB {
 			return properties;
 		} catch (IOException e) {
 			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatement(Statement statement) {
+		if(statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet resultSet) {
+		if(resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
 	}
 }
--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,14 +1,34 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import Classes.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection connection = DB.getConnection();
-		DB.closeConnection();
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			connection = DB.getConnection();
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("select * from department");
+
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeResultSet(resultSet);
+			DB.closeStatement(statement);
+			DB.closeConnection();
+		}
 	}
 
 }
